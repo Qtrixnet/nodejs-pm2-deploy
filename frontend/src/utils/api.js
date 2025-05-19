3735ca010ad9667a0dd6d8bcc12ca6bd31e5e3ce
Kirill <qtrixnet@yandex.ru>
2025-05-20 00:08:24 +0500

    fix api path

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://qtrixnet.mesto.nomorepartiessbs.ru');
+  const api = new Api('https://api.qtrixnet.mesto.nomorepartiessbs.ru');
 
   export default api;
